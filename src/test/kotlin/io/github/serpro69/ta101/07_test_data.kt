75cf59defd9b980cd5a8346c02d84acaed2bc406
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-09-28 17:55:02 +0200

    Add exercise 07 (wip)

---
+++ src/test/kotlin/io/github/serpro69/ta101/07_test_data.kt
@@ -0,0 +1,68 @@
+package io.github.serpro69.ta101
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
+import io.restassured.RestAssured.given
+import io.restassured.config.ObjectMapperConfig
+import io.restassured.config.RestAssuredConfig
+import io.restassured.http.ContentType
+import io.restassured.specification.RequestSpecification
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+private val request: () -> RequestSpecification = {
+    val cfg = RestAssuredConfig.config()
+        .objectMapperConfig(ObjectMapperConfig.objectMapperConfig().jackson2ObjectMapperFactory { _, _ ->
+            jacksonMapperBuilder()
+                .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true)
+                .configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
+                .build()
+        })
+    given()
+        .config(cfg)
+        .baseUri("http://localhost:8080/api/v3")
+}
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Tests07 {
+
+    @ParameterizedTest
+    @MethodSource("petSource")
+    fun `test parameterized post`(pet: Pet) {
+        val response = request()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(pet)
+            .`when`()
+            .post("/pet")
+            .then()
+
+        response.statusCode(200)
+        response.body("id", equalTo(42))
+    }
+
+    private fun petSource(): Stream<Pet> {
+        return Stream.of(
+            Pet(
+                42,
+                "mango",
+                Category(1, "Dogs"),
+                emptyList(),
+                emptyList(),
+                Status.AVAILABLE
+            ),
+            Pet(
+                73,
+                "kiwi",
+                Category(1, "Dogs"),
+                emptyList(),
+                emptyList(),
+                Status.AVAILABLE
+            )
+        )
+    }
+}
