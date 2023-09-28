afae1aeb2310d0b79f8a4af5924cc5d20e51f0b8
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-09-28 18:06:10 +0200

    Complete exercise 07

--- src/test/kotlin/io/github/serpro69/ta101/07_test_data.kt
+++ src/test/kotlin/io/github/serpro69/ta101/07_test_data.kt
@@ -3,6 +3,7 @@ package io.github.serpro69.ta101
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
+import io.github.serpro69.kfaker.faker
 import io.restassured.RestAssured.given
 import io.restassured.config.ObjectMapperConfig
 import io.restassured.config.RestAssuredConfig
@@ -64,5 +65,34 @@ class Tests07 {
                 Status.AVAILABLE
             )
         )
+    }
+
+    @ParameterizedTest
+    @MethodSource("fakerPetSource")
+    fun `test parameterized post with fake data generation`(pet: Pet) {
+        val response = request()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(pet)
+            .`when`()
+            .post("/pet")
+            .then()
+
+        response.statusCode(200)
+        response.body("id", equalTo(42))
+    }
+
+    private fun fakerPetSource(): Stream<Pet> {
+        val faker = faker {  }
+        val dogs = List(10) {
+            faker.randomProvider.randomClassInstance<Pet> {
+                namedParameterGenerator("id") { faker.random.nextInt(100, 1000) }
+                namedParameterGenerator("name") { faker.dog.name() }
+                typeGenerator<Category> {
+                    Category(1, "Dogs")
+                }
+            }
+        }
+        return dogs.stream()
     }
 }
